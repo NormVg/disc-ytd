4e99544885f302262d7c8676404ddb091a9c4c66
NormVg <87635928+NormVg@users.noreply.github.com>
2024-03-27 12:55:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask,send_file,request
+
 from pytube import YouTube
-from pydub import AudioSegment
+from moviepy.editor import *
 import tempfile
-import os
+
+def download_mp3(url):
+    mp4 =  tempfile.NamedTemporaryFile(suffix='.mp4', delete=False) 
+    yt  = YouTube(url)
+    print("downloading")
+    file = yt.streams.get_audio_only().download(filename=mp4.name)
+    print("downloaded")
+    mp3 =  tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) 
+    AudioFileClip(file).write_audiofile(mp3.name)
+    return mp3.name
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    files = os.listdir(tempfile.gettempdir())
-    mp3_files = [f for f in files if f.endswith('.mp3')]
-    return render_template('index.html', mp3_files=mp3_files)
+@app.get("/")
+def inedx():
+    url = request.args.get("url")
+    file = download_mp3(url)
+    return send_file(file)
 
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form['url']
-    
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
-    
-    if video:
-        audio_stream = video.download(output_path=tempfile.gettempdir())
-        
-        # Convert to MP3 using pydub
-        audio_path = audio_stream[:-4] + '.mp3'
-        AudioSegment.from_file(audio_stream).export(audio_path, format='mp3')
-        
-        os.remove(audio_stream)  # Remove original audio file
-        
-        return audio_path.split('/')[-1]
-    
-    return 'Error downloading the video'
-
-@app.route('/play/<path:file_name>')
-def play(file_name):
-    return send_file(os.path.join(tempfile.gettempdir(), file_name), as_attachment=True, mimetype='audio/mp3')
-
-if __name__ == '__main__':
-    app.run()
+if __name__=="__main__":
+    app.run(debug=True)
