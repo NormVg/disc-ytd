e6e6943fac0954d61373694d49964c5bec76db88
NormVg <87635928+NormVg@users.noreply.github.com>
2024-03-27 13:24:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,21 @@ from flask import Flask,send_file,request,jsonify
 from pytube import YouTube
 from moviepy.editor import *
 import tempfile
+import eyed3
+import requests
+
 
 def download_mp3(url):
     mp4 =  tempfile.NamedTemporaryFile(suffix='.mp4', delete=False) 
     yt  = YouTube(url)
-    print("downloading")
-    file = yt.streams.get_audio_only().download(filename=mp4.name)
-    print("downloaded")
+    file = yt.streams.get_lowest_resolution().download(filename=mp4.name)
     mp3 =  tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) 
     AudioFileClip(file).write_audiofile(mp3.name)
+    audiofile = eyed3.load(mp3.name)
+    audiofile.initTag(version=(2, 3, 0))
+    imagedata = requests.get(yt.thumbnail_url).content
+    audiofile.tag.images.set(3, imagedata, "image/jpeg", u"cover")
+    audiofile.tag.save()
     return mp3.name
 
 app = Flask(__name__)
